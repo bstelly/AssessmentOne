e34ee1d17de73687d734f39bbfb58f59ba46476a
bstelly <bstelly1999@gmail.com>
2017-10-10 14:30:21 -0500

    Finished Tests

---
+++ Wumpus/Commands.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "MyString.h"
+#include "Player.h"
+
--- Wumpus/Test.h
+++ Wumpus/Test.h
@@ -1,31 +1,12 @@
 #pragma once
 #include "MyString.h"
+#include "Test.h"
+#include <iostream>
 #include <fstream>
+#include <assert.h>
 using namespace std;
 
-fstream testFile;
-void StringTestSuccess(int testNum, const char* test)
-{
-	if (testFile.is_open())
-	{
-		testFile << "Test	" << testNum << "	" << test << "	Successful" << endl;
-	}
-	else
-	{
-		testFile.open("Test.txt", ios_base::out);
-		testFile << "Test	" << testNum << "	" << test << "	Successful" << endl;
-	}
-}
 
-void StringTestFail(int testNum, const char* test)
-{
-	if (testFile.is_open())
-	{
-		testFile << "Test	" << testNum << "	" << test << "	Failed" << endl;
-	}
-	else
-	{
-		testFile.open("Test.txt", ios_base::out);
-		testFile << "Test	" << testNum << "	" << test << "	Failed" << endl;
-	}
-}
+void StringTestSuccess(int testNum, const char* test);
+void StringTestFail(int testNum, const char* test);
+void RunFunctionTest();
